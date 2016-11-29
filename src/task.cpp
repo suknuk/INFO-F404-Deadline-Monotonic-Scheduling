c7f1537e4d52645916a10efa3bbf5d8be8e9d79b
Raymond Lochner <ray_lochner@yahoo.de>
2016-11-29 14:40:19 +0100

    Improved task priority sorting

--- src/task.cpp
+++ src/task.cpp
@@ -40,7 +40,33 @@ int Task::get_wcet() const
 
 bool operator<(const Task &t1, const Task &t2)
 {
-	return t1.get_deadline() < t2.get_deadline();
+	// sort by deadline first as it is DM
+	if (t1.get_deadline() < t2.get_deadline()){
+		return true;
+	} else if (t1.get_deadline() > t2.get_deadline()) {
+		return false;
+	// ow deadlines are equal
+	} else {
+		// utilization secondly
+		if (t1.calculate_utilization() > t2.calculate_utilization()) { 	
+			return  true; 
+			
+		} else if (t1.calculate_utilization() > t2.calculate_utilization()) {
+			return false;
+		// utilization also equal
+		} else {
+			//offset thirdly
+			if (t1.get_offset() < t2.get_offset()) {
+				return true;
+			} else if (t1.get_offset() > t2.get_offset()) {
+				return false;
+			// ow they are equal in deadline, utilization and offset
+			} else {
+				return true;
+			}
+		}
+	}
+	//old-- return t1.get_deadline() < t2.get_deadline();
 }
 
 
