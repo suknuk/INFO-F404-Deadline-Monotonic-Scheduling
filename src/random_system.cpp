7f19673f4d6945eccf4ac7fbcc85abddd2ff8de9
Raymond Lochner <ray_lochner@yahoo.de>
2016-12-11 18:45:50 +0100

    Random tasks

--- src/random_system.cpp
+++ src/random_system.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 #include "random_system.h"
 
 void RandomSystem::generate_system()
 {
 	std::srand(std::time(0));
-	// create a random period between 10 and 100
+	// create a random max_Period between 10 and 100
 	int max_period = std::rand() % 90 + 10;
-	std::cout << max_period << std::endl;
+	std::cout << "Max period = " << max_period << std::endl;
+	
+	// holds all tasks
+	//std::vector<Task> schedule;
+	for (int i = 0; i < this->_tasks; i++) {
+		
+		int offset = std::rand() % (max_period/2);
+		
+		int period = std::rand() % max_period + 10;
+
+		// has to be at least 1
+		int wcet = std::rand() % (max_period-1) + 1;
+
+		int deadline = std::rand() % (period - wcet) + wcet;
+
+		std::cout << "offset : " << offset << ", period: " << period
+			<< ", deadline: " << deadline << ", wcet: " << wcet
+			<< std::endl;
+
+		//Task task
+	}
+
 }
 
 RandomSystem::RandomSystem(int tasks, int utilization)
